ae3e288a3e0b31bc8e83de449ee2c0afbf8579ee
Thierry K <thierry.kamagne@gmail.com>
2023-10-27 09:42:40 +0200

    Terraform migrate local state to Cloud (#12)

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 terraform {
-  required_providers {
+
+
+    cloud {
+        organization = "kamagne"
+        workspaces {
+          name = "terra-house-tka"
+        }
+      }
+    required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
